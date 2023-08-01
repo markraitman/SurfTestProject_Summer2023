f7d2ae239676864e6597c069e929dc610c940b1a
Mark Raitman <markraitman@mail.ru>
2023-08-01 14:42:07 +0300

    Navigation title
    - Add navigation title
    - Sort project files into proper folders

--- SurfTestProject_Summer2023/App/SceneDelegate.swift
+++ SurfTestProject_Summer2023/App/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
