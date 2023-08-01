ecc89ed936994dfa33b69b828ef993f661241e48
Mark Raitman <markraitman@mail.ru>
2023-08-01 12:29:59 +0300

    Initial Commit

---
+++ SurfTestProject_Summer2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SurfTestProject_Summer2023
+//
+//  Created by Марк Райтман on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
